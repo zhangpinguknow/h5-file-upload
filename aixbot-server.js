8cc838bc49a78ea90be78ff0d827d42658b7d447
张萍 <zhangping@tuhu.cn>
2019-03-08 11:12:55 +0800

    小爱测试

---
+++ aixbot-server.js
@@ -0,0 +1,48 @@
+ const AixBot = require('../aixbot');
+
+const aixbot = new AixBot("419804200885554176");
+
+// define middleware
+aixbot.use(async (ctx, next) => {
+    console.log(`process request for '${ctx.request.query}' ...`);
+    var start = new Date().getTime();
+    await next();
+    var execTime = new Date().getTime() - start;
+    console.log(`... response in duration ${execTime}ms`);
+});
+
+aixbot.use(async (ctx, next) => {
+    ctx.db = {
+        username : 'Bowen'
+    };
+    await next();
+});
+
+// define event handler
+aixbot.onEvent('enterSkill', (ctx) => {
+    ctx.query('你好');
+});
+
+// define text handler
+aixbot.hears('你是谁', (ctx) => {
+    ctx.speak(`我是${ctx.db.username}`).wait();
+});
+
+// define regex handler
+aixbot.hears(/\W+/, (ctx) => {
+    ctx.speak(ctx.request.query);
+});
+
+// close session
+aixbot.onEvent('quitSkill', (ctx) => {
+    ctx.reply('再见').closeSession();
+});
+
+// define error handler
+aixbot.onError((err, ctx) => {
+    logger.error(`error occurred: ${err}`);
+    ctx.reply('内部错误，稍后再试').closeSession();
+});
+
+// run http server
+aixbot.run(8080);
